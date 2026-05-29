8e59e08732ecb4fb80b0d28cd6ec659a398ff42d
Leruva <sjruva2006@gmail.com>
2026-05-29 16:04:04 +0530

    updated authentication for club

--- backend/modules/auth/auth_router.js
+++ backend/modules/auth/auth_router.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const controller = require('./auth_controller');
-
+const {verifyToken} = require('../../middleware/auth_middleware');
 const router = express.Router();
 
 router.post('/student/register', controller.studentRegister);
 router.post('/student/login', controller.studentLogin);
-router.post('/club/register', controller.clubRegister);
+router.post('/club/register', verifyToken, controller.clubRegister);
 router.post('/club/login', controller.clubLogin);
 
 module.exports = router;
