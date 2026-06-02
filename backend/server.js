8f693cd3919e0eebf35314b69ee3af12ea86b399
Leruva <sjruva2006@gmail.com>
2026-06-02 21:06:59 +0530

    add cors

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const connectDB = require('./config/dbConnection');
 require('dotenv').config();
 const PORT = process.env.PORT || 5001;
@@ -6,6 +7,7 @@ const app = express();
 connectDB();
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/api/auth', require('./modules/auth/auth_router'));
 app.use('/api/admin', require('./modules/admin/admin_routes'));
